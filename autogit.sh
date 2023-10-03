d333d0f07fc69bf2f2f064fe4a3a1e6db0087e10
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-03 12:24:26 -0300

    Deployed successfully

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,4 @@
-git init --initial-branch=main
-git remote add origin https://gitlab.com/CarlosViniMSouza/cvmds-blog.git
-git add .
-git commit -m "Initial commit"
-git push --set-upstream origin main
+git add -A
+git commit -m "Deployed successfully"
+git push -u origin main
 git status
